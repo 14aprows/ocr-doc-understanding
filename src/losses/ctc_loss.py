0ea742a72fc6eba85e7508ef0e44da6196ed9544
lwkys <Hasibuanfirman1404@gmail.com>
2026-06-03 15:50:16 +0700

    add ctc loss for crnn ocr

---
+++ src/losses/ctc_loss.py
@@ -0,0 +1,26 @@
+import torch 
+from torch._inductor.config import batch_fusion
+import torch.nn as nn
+
+class CTCLosCRNNCTCLosss(nn.Module):
+    def __init__(self, blank_idx=0):
+        super().__init__()
+        self.loss_fn = nn.CTCLoss(
+            blank=blank_idx,
+            zero_infinity=True        
+        )
+
+    def forward(self, logits, targets, target_lengths):
+        log_probs = logits.log_softmax(dim=2)
+        time_steps = log_probs.size(0)
+        batch_size = log_probs.size(1)
+        
+        input_lengths = torch.full(
+            size = (batch_size, ),
+            fill_value = time_steps,
+            dtype=torch.long,
+            device=log_probs.device
+        )
+
+        loss = self.loss_fn(log_probs, targets, input_lengths, target_lengths)
+        return loss
