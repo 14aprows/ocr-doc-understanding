efb498fffd0aab3d1828653d465052fa6c6f33fc
lwkys <Hasibuanfirman1404@gmail.com>
2026-06-06 17:43:34 +0700

    add CER validation metric

--- src/engine/trainer.py
+++ src/engine/trainer.py
@@ -1,4 +1,5 @@
 import torch 
+from src.metrics.cer import average_cer
 
 def train_one_epoch(model, dataloader, criterion, optimizer, device):
     model.train()
@@ -16,9 +17,12 @@ def train_one_epoch(model, dataloader, criterion, optimizer, device):
         train_loss += loss.item()
     return train_loss / len(dataloader)
 
-def validate_one_epoch(model, dataloader, criterion, device):
+def validate_one_epoch(model, dataloader, criterion, device, encoder=None):
     model.eval()
     val_loss = 0.0
+    all_preds = []
+    all_targets = []
+
     with torch.no_grad():
         for batch in dataloader:
             images = batch["images"].to(device)
@@ -27,4 +31,18 @@ def validate_one_epoch(model, dataloader, criterion, device):
             logits = model(images)
             loss = criterion(logits, targets, target_lengths)
             val_loss += loss.item()
-    return val_loss / len(dataloader)
+
+            if encoder is not None:
+                pred_indices = logits.argmax(dim=2)
+                for i in range(pred_indices.size(1)):
+                    pred = pred_indices[:, i]
+                    pred_text = encoder.decode(pred)
+                    all_preds.append(pred_text)
+                all_targets.extend(batch["texts"])
+
+    avg_loss = val_loss / len(dataloader)
+    if encoder is None:
+        return avg_loss
+
+    cer = average_cer(all_preds, all_targets)
+    return avg_loss, cer
