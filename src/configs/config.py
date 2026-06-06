1cebbe65049366e0a0c925b035823f750d237a79
lwkys <Hasibuanfirman1404@gmail.com>
2026-06-06 18:10:14 +0700

    add checkpoint and log dir config

--- src/configs/config.py
+++ src/configs/config.py
@@ -27,7 +27,7 @@ BEST_MODEL_PATH = CHECKPOINT_DIR / "best_ocr_model.pth"
 LAST_MODEL_PATH = CHECKPOINT_DIR / "last_ocr_model.pth"
 
 LOG_DIR = PROJECT_ROOT / "logs"
-TRAIN_LOG_PATH = LOG_DIR / "ocr_training_log.csv"
+# TRAIN_LOG_PATH = LOG_DIR / "ocr_training_log.csv"
 
 OCR_IMAGE_HEIGHT = 32
 OCR_IMAGE_WIDTH = 128
